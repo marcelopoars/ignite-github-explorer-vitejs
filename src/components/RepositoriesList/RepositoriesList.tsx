4214dcf85e9f1df2728579321b74d8dd8c0c7e3a
Marcelo Pereira <x654766@gruposantander.com>
2023-03-01 19:08:53 -0300

    refactor: refactored the RepositoriesPage

--- src/components/RepositoriesList/RepositoriesList.tsx
+++ src/components/RepositoriesList/RepositoriesList.tsx
@@ -1,34 +1,19 @@
-import { useEffect, useState } from "react";
+import { Repository } from "../../Models";
+import { RepositoryItem } from "../RepositoryItem";
 
-import { RepositoryItem } from "../RepositoryItem/RepositoryItem";
-import { Repository } from "../RepositoryItem/interfaces";
-import { Loading } from "../Loading";
+interface RepositoriesListProps {
+  repositories: Repository[];
+}
 
-export const RepositoriesList = () => {
-  const [repositories, setRepositories] = useState<Repository[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("https://api.github.com/users/marcelopoars/repos").then((response) =>
-      response.json().then((data: Repository[]) => {
-        setRepositories(data);
-        setIsLoading(false);
-      })
-    );
-  }, []);
-
+export const RepositoriesList = ({ repositories }: RepositoriesListProps) => {
   return (
     <section className="container py-8">
       <h1>My GitHub Repositories</h1>
 
       <ul className="pt-8">
-        {isLoading ? (
-          <Loading text="Buscando repositórios..." />
-        ) : (
-          repositories.map((repository) => (
-            <RepositoryItem key={repository.name} repository={repository} />
-          ))
-        )}
+        {repositories.map((repository) => (
+          <RepositoryItem key={repository.name} repository={repository} />
+        ))}
       </ul>
     </section>
   );
